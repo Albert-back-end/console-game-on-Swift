6af3099aa6ddb78f815d9824054ccc8d72c44c62
Albert <albert231207@mail.ru>
2024-11-24 22:50:48 +0300

    Add logic for all games

---
+++ Swift Game/Swift Game/GameEngine.swift
@@ -0,0 +1,39 @@
+//
+//  GameEngine.swift
+//  Swift Game
+//
+//  Created by Albert on 11/24/24.
+//
+
+import Foundation
+
+
+class Engine {
+    let countCycle = 3
+
+    func engineGame(mainQuestion: String, rounds: [GameRound]) {
+        print(mainQuestion)
+
+        for round in rounds {
+            let mainQuestion = "Question: \(round.getQuestion())"
+            print(mainQuestion)
+
+            print("Your answer: ", terminator: "")
+            guard let userAnswer = readLine() else {
+                continue
+            }
+
+            let answer = round.getAnswer()
+
+            if userAnswer != answer {
+                let errorMessage = "'\(userAnswer)' is wrong answer ;(. Correct answer was '\(answer)'. Let's try again, \(userName)!"
+                print(errorMessage)
+                exit(0)
+            }
+            print("Correct!")
+        }
+
+        print("Congratulations, \(userName)!")
+    }
+}
+
---
+++ Swift Game/Swift Game/GameRound.swift
@@ -0,0 +1,22 @@
+//
+//  GameRound.swift
+//  Swift Game
+//
+//  Created by Albert on 11/24/24.
+//
+
+import Foundation
+
+
+struct GameRound {
+    let question: String
+    let answer: String
+    
+    func getQuestion() -> String {
+        return question
+    }
+    
+    func getAnswer() -> String {
+        return answer
+    }
+}
